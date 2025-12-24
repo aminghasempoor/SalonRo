87451f3d191647e3e8faf2a56d4559add2a54d13
AminGhasempoor <aminghasempoor79@gmail.com>
2025-12-24 12:04:45 +0330

    add pwa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import { NextConfig } from "next";
+import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
-
-const nextConfig: NextConfig = {};
+import nextPWA from "next-pwa";
 
 const withNextIntl = createNextIntlPlugin();
-export default withNextIntl(nextConfig);
+
+const withPWA = nextPWA({
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+    register: true,
+    skipWaiting: true,
+    navigateFallback: "/offline.html",
+});
+
+const nextConfig: NextConfig = {};
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore -- no type for intl
+export default withPWA(withNextIntl(nextConfig));
